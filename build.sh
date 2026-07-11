31af73a9d9251f4a85b129802e03be5a18b52cdd
Dylan Eck <akdylaneck@gmail.com>
2026-07-11 05:41:55 -0400

    improve build script

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+set -euo pipefail
 
-mkdir -p build/$1
-cmake --preset $1
-cmake --build build/$1
-ln -sf build/$1/compile_commands.json compile_commands.json
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <preset>"
+    exit 1
+fi
+
+mkdir -p "build/$1"
+cmake --preset "$1"
+cmake --build "build/$1"
+ln -sf "build/$1/compile_commands.json" compile_commands.json
